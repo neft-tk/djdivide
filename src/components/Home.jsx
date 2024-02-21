9617da9ae6cde48c1b170202a468a16092ef6529
Erik <ebussman@gmail.com>
2024-02-21 10:43:06 -0800

    updated css styling for bg and text colors first draft

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,37 +3,37 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col bg-gray-100">
+      <div className="flex flex-col bg-black">
         <div className="mt-8 md:p-16 flex flex-col justify-center md:justify-start text-white recital-bg">
-          <h1 className="flex justify-center">Cecilia Poellein Buss</h1>
-          <h1 className="flex text-center justify-center">
-            Violinist/Violin and Viola Instructor
-          </h1>
+          <h1 className="flex justify-center">Dj Divide</h1>
+          {/* <h1 className="flex text-center justify-center">
+            DJ Expert *insert type of music maybe?*
+          </h1> */}
         </div>
         <p>
-        Hello world, my name is Jake and I am a DJ based out of Seattle, WA.
-Born and raised in Seattle, I started out as a guitarist and I have been interested in music for as long as I can remember.
-I have been DJing professionally since 2017 and I absolutely love it!  It is my passion to share music with people.
-I own and operate top of the line, industry standard multi-players and a mixer and a quality speaker system.
-I have been lucky enough to DJ a wide variety of venues including cannabis shops, bars and private events.
-I have a working vehicle and I can transport my gear to where you need the music played, so if you live in an area outside of Seattle, that's no problem!
-My music collection is ever expanding so I can DJ a lot of different genres, but I specialize in EDM, hip/hop & rap, and hard rock/metal.
-If you would like to know more, don't hesitate to call/text/email/DM!</p>
-<p>
-My philosophy is to play 50% for the crowd, and 50% for myself.  Even Steven.
-I want your event to be awesome and I want the audience to have a great time.
-Take a look at my rates page, but keep in mind I am open to negotiations about price of my services.
-Again, anything else you wanna know, don't hesitate to call/text/email/DM.
-</p>
-<p>
-Keep on rocking in the free world,
+          Hello world, my name is Jake and I am a DJ based out of Seattle, WA.
+          Born and raised in Seattle, I started out as a guitarist and I have been interested in music for as long as I can remember.
+          I have been DJing professionally since 2017 and I absolutely love it!  It is my passion to share music with people.
+          I own and operate top of the line, industry standard multi-players and a mixer and a quality speaker system.
+          I have been lucky enough to DJ a wide variety of venues including cannabis shops, bars and private events.
+          I have a working vehicle and I can transport my gear to where you need the music played, so if you live in an area outside of Seattle, that's no problem!
+          My music collection is ever expanding so I can DJ a lot of different genres, but I specialize in EDM, hip/hop & rap, and hard rock/metal.
+          If you would like to know more, don't hesitate to call/text/email/DM!</p>
+        <p>
+          My philosophy is to play 50% for the crowd, and 50% for myself.  Even Steven.
+          I want your event to be awesome and I want the audience to have a great time.
+          Take a look at my rates page, but keep in mind I am open to negotiations about price of my services.
+          Again, anything else you wanna know, don't hesitate to call/text/email/DM.
+        </p>
+        <p>
+          Keep on rocking in the free world,
 
-- Jake ➗
+          - Jake ➗
         </p>
 
         <div className="mx-4 flex flex-col items-center justify-center" id="contact">
           <h2 className="mb-5">
-          Cell: 206.427.1555
+            Cell: 206.427.1555
           </h2>
           <h2 className="mb-5">
             Email: djdivide1@gmail.com
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,13 +6,13 @@ export default function Nav({ handleLogout, isLoggedIn }) {
   const onLogoutClick = () => {
     handleLogout();
   }
-  
+
   return (
     <nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div class="container flex flex-wrap items-center justify-between mx-none">
         <a href="/" class="flex items-center">
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            Cecilia Poellein Buss' Violin Studio
+            DJ Divide
           </span>
         </a>
         <div
@@ -21,14 +21,14 @@ export default function Nav({ handleLogout, isLoggedIn }) {
         >
           <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <Link to="/">
+              <a href="#home">
                 <h1
                   className="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:text-black md:p-0 dark:text-white"
                   aria-current="page"
                 >
                   Home
                 </h1>
-              </Link>
+              </a>
             </li>
             {/* <li>
               <div>
