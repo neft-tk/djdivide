ae4fa9f73c3a0036a4941a4fddc565ef5d283067
Erik <ebussman@gmail.com>
2024-03-07 13:35:11 -0800

    updated nav bg colors for netlify

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -19,7 +19,7 @@ export default function Nav({ handleLogout, isLoggedIn }) {
           class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
         >
-          <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-black dark:bg-black md:dark:bg-black dark:border-gray-700">
             <li>
               <a href="#home">
                 <h1
