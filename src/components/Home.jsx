05008e4580e5b48481ef5e38d4b238c05a9dfe96
Erik <ebussman@gmail.com>
2024-03-09 11:37:20 -0800

    added more margins to the homepage for divs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,28 +12,31 @@ export default function Home() {
           </h2>
           <img src={JSLogo1} alt="JSLogo" className="flex max-w-md justify-center" />
         </div>
-        <p>
-          Hello world, my name is Jake and I am a DJ based out of Seattle, WA.
-          Born and raised in Seattle, I started out as a guitarist and I have been interested in music for as long as I can remember.
-          I have been DJing professionally since 2017 and I absolutely love it!  It is my passion to share music with people.
-          I own and operate top of the line, industry standard multi-players and a mixer and a quality speaker system.
-          I have been lucky enough to DJ a wide variety of venues including cannabis shops, bars and private events.
-          I have a working vehicle and I can transport my gear to where you need the music played, so if you live in an area outside of Seattle, that's no problem!
-          My music collection is ever expanding so I can DJ a lot of different genres, but I specialize in EDM, hip/hop & rap, and hard rock/metal.
-          If you would like to know more, don't hesitate to call/text/email/DM!</p>
-        <p>
-          My philosophy is to play 50% for the crowd, and 50% for myself.  Even Steven.
-          I want your event to be awesome and I want the audience to have a great time.
-          Take a look at my rates page, but keep in mind I am open to negotiations about price of my services.
-          Again, anything else you wanna know, don't hesitate to call/text/email/DM.
-        </p>
-        <p>
-          Keep on rocking in the free world,
+        <div className="my-8">
+          <p>
+            Hello world, my name is Jake and I am a DJ based out of Seattle, WA.
+            Born and raised in Seattle, I started out as a guitarist and I have been interested in music for as long as I can remember.
+            I have been DJing professionally since 2017 and I absolutely love it!  It is my passion to share music with people.
+            I own and operate top of the line, industry standard multi-players and a mixer and a quality speaker system.
+            I have been lucky enough to DJ a wide variety of venues including cannabis shops, bars and private events.
+            I have a working vehicle and I can transport my gear to where you need the music played, so if you live in an area outside of Seattle, that's no problem!
+            My music collection is ever expanding so I can DJ a lot of different genres, but I specialize in EDM, hip/hop & rap, and hard rock/metal.
+            If you would like to know more, don't hesitate to call/text/email/DM!
+          </p>
+          <p>
+            My philosophy is to play 50% for the crowd, and 50% for myself.  Even Steven.
+            I want your event to be awesome and I want the audience to have a great time.
+            Take a look at my rates page, but keep in mind I am open to negotiations about price of my services.
+            Again, anything else you wanna know, don't hesitate to call/text/email/DM.
+          </p>
+          <p>
+            Keep on rocking in the free world,
 
-          - Jake ➗
-        </p>
+            - Jake ➗
+          </p>
+        </div>
 
-        <div className="mx-4 flex flex-col items-center justify-center" id="contact">
+        <div className="m-8 flex flex-col items-center justify-center" id="contact">
           <h2 className="mb-5">
             Cell: 206.427.1555
           </h2>
@@ -53,15 +56,15 @@ However I would strongly prefer the cash. */}
 
 
 
-        <div className="mx-4 flex flex-col items-center justify-center" id="rates">
+        <div className="m-8 flex flex-col items-center justify-center" id="rates">
           <h1 className="mb-5">
             Rates
           </h1>
           <h2 className="mb-5">
-          Thus far I have been charging $400 for a five hour set
+            Thus far I have been charging $400 for a five hour set
           </h2>
           <h2 className="mb-5">
-          I am open to getting compensated in store credit, however I would strongly prefer cash
+            I am open to getting compensated in store credit, however I would strongly prefer cash
           </h2>
           <h2 className="mb-5">Instagram: rickyrakehell</h2>
         </div>
